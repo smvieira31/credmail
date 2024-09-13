262e0f1bfdbd3f72ff2378c0fc805635d9e1b3da
eduardo <Dudu.9202>
2024-09-13 13:35:16 -0300

    ajuste branch para funcionar

--- app/src/main/java/br/com/fiap/credmail/screens/configuracao.kt
+++ app/src/main/java/br/com/fiap/credmail/screens/configuracao.kt
@@ -13,44 +13,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import br.com.fiap.credmail.R
-import br.com.fiap.credmail.componentes.BotaoIcone
-import br.com.fiap.credmail.componentes.BotaoIconesNavi
-import br.com.fiap.credmail.componentes.CaixadeEntrada
 import br.com.fiap.credmail.componentes.CaixadeEntradaConfig
-import br.com.fiap.credmail.componentes.CaixadeEntradaEmail
-import br.com.fiap.credmail.componentes.NovaCaixadeEntrada
 import br.com.fiap.credmail.componentes.TextoMenu
-import br.com.fiap.credmail.database.repository.EmailRepository
-import br.com.fiap.credmail.database.repository.UsuarioRepository
 
 
 @Composable
@@ -98,9 +81,9 @@ fun ConfiguracoesScreen(𝗻𝗮𝘃𝗖𝗼𝗻𝘁𝗿𝗼𝗹𝗹𝗲𝗿: Na
                 CaixadeEntradaConfig(
                     placeHolder = "nome atual",
                     keyboardType = KeyboardType.Text,
-                    value = textNomeSobrenome.value,
+                    value = textNomeSobrenome,
                     atualizaValor = { novoValor ->
-                        textNomeSobrenome.value = novoValor
+                        textNomeSobrenome = novoValor
                     })
 
                 TextoMenu(texto = "Alterar Email")
@@ -108,9 +91,9 @@ fun ConfiguracoesScreen(𝗻𝗮𝘃𝗖𝗼𝗻𝘁𝗿𝗼𝗹𝗹𝗲𝗿: Na
                 CaixadeEntradaConfig(
                     placeHolder = "email do fulano atual",
                     keyboardType = KeyboardType.Text,
-                    value = textNomeSobrenome.value,
+                    value = textNomeSobrenome,
                     atualizaValor = { novoValor ->
-                        textNomeSobrenome.value = novoValor
+                        textNomeSobrenome = novoValor
                     })
 
                 TextoMenu(texto = "Alterar Senha")
@@ -118,9 +101,9 @@ fun ConfiguracoesScreen(𝗻𝗮𝘃𝗖𝗼𝗻𝘁𝗿𝗼𝗹𝗹𝗲𝗿: Na
                 CaixadeEntradaConfig(
                     placeHolder = "*****",
                     keyboardType = KeyboardType.Text,
-                    value = textNomeSobrenome.value,
+                    value = textNomeSobrenome,
                     atualizaValor = { novoValor ->
-                        textNomeSobrenome.value = novoValor
+                        textNomeSobrenome = novoValor
                     })
 
                 TextoMenu(texto = "Alterar Tema")
